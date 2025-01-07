913e9567fe4807484e81f7145251fd5faf314532
SeongJae Park <sj@kernel.org>
2025-01-06 17:29:08 -0800

    eval.sh: simplify output
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- eval.sh
+++ eval.sh
@@ -9,7 +9,8 @@ hint=4	#MADV_DONTNEED
 nr_measures=100
 sz_mem=$((256*1024*1024))	# 256 MiB
 
-echo "<hint> <sz_mem> <sz_madv> <pmadv_batch> <measure_repeat> <madv|pmadv> <latency>"
+echo "# <nr_measures> 100"
+echo "# <hint> <sz_mem> <sz_madv> <pmadv_batch> <madv|pmadv> <latency>"
 
 for sz_madv_pg in 1 2 4 8 16 32 64 128 256 512 1024
 do
@@ -17,14 +18,14 @@ do
 	latency=$(./eval_madv "$hint" "$sz_mem" "$sz_madv" "$sz_p_madv" \
 		"$nr_measures")
 	pmadv_batch="n/a"
-	echo "$hint $sz_mem $sz_madv $pmadv_batch $nr_measures madv $latency"
+	echo "$hint $sz_mem $sz_madv $pmadv_batch madv $latency"
 
 	for pmadv_batch in 1 2 4 8 16 32 64 128 256 512 1024
 	do
 		sz_p_madv=$((pmadv_batch * sz_madv))
 		latency=$(./eval_pmadv "$hint" "$sz_mem" "$sz_madv" "$sz_p_madv" \
 			"$nr_measures")
-		echo "$hint $sz_mem $sz_madv $pmadv_batch $nr_measures pmadv $latency"
+		echo "$hint $sz_mem $sz_madv $pmadv_batch pmadv $latency"
 
 	done
 done
